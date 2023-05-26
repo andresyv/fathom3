147ef9dddcc9022cebc31b9ba946fc8d76c99945
andresyv <andresyv93@outlook.es>
2023-05-26 23:45:53 +0200

    CHORE(settings): initial project setup

--- apps/frontend/.eslintrc.cjs
+++ apps/frontend/.eslintrc.cjs
@@ -1,14 +1,24 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    './node_modules/ts-standard/eslintrc.json'
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  plugins: ["react-refresh"],
-  rules: {
-    "react-refresh/only-export-components": "warn",
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json'
   },
-};
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    'jsx-quotes': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off'
+  }
+}
