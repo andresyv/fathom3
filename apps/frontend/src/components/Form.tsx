4baf36bb8717be9ff8dc82891152a7f0c53b6370
andresyv <andresyv93@outlook.es>
2023-05-27 20:29:59 +0200

    FEAT(Forms): form and input component

---
+++ apps/frontend/src/components/Form.tsx
@@ -0,0 +1,22 @@
+import React, { type FormHTMLAttributes, type ReactElement } from 'react'
+import { useForm, SubmitHandler, FieldValues, UseFormProps, FormProvider } from 'react-hook-form'
+
+interface FormProps<T extends FieldValues> extends Omit<FormHTMLAttributes<HTMLFormElement>, 'onSubmit'> {
+  children: ReactElement | ReactElement[]
+  defaultValues?: UseFormProps<T>
+  onSubmit: SubmitHandler<T>
+}
+
+const SForm = <T extends FieldValues>({ defaultValues, children, onSubmit, className }: FormProps<T>) => {
+  const methods = useForm<T>(defaultValues)
+  const { handleSubmit } = methods
+  return (
+    <FormProvider {...methods}>
+      <form className={className} onSubmit={handleSubmit(onSubmit)}>
+        {children}
+      </form>
+    </FormProvider>
+  )
+}
+
+export default SForm
---
+++ apps/frontend/src/components/Input.tsx
@@ -0,0 +1,20 @@
+import type { FC, InputHTMLAttributes } from 'react'
+import { useFormContext, FieldValues, Path } from 'react-hook-form'
+
+interface InputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'id' | 'className'> {
+  name: Path<FieldValues>
+}
+const SInput: FC<InputProps> = ({ name, ...rest }) => {
+  const { register } = useFormContext()
+  return (
+    <fieldset className="flex flex-col w-full">
+      <input
+        className="border rounded-lg outline-none border-2 border-gray-300 text-md py-3.5 px-4 placeholder-gray-500 transition-all focus:ring focus:ring-indigo-300"
+        {...register(name)}
+        {...rest}
+      />
+    </fieldset>
+  )
+}
+
+export default SInput
