727070dc9f13d704374874634dc7d67319f1bfee
Andrés Yepes <andres@passporterapp.com>
2023-05-28 19:13:53 +0200

    FEAT(CustomFetch): temp commit custom fetch

---
+++ apps/frontend/src/services/auth.ts
@@ -0,0 +1,6 @@
+import { LoginFormFields } from '../types'
+import customFetch from './custom-fetch'
+
+const BASE_URL = 'http://localhost:8080'
+
+function login(form: LoginFormFields) {}
---
+++ apps/frontend/src/services/custom-fetch.ts
@@ -0,0 +1,44 @@
+const allowedOrigins = ['localhost:5173']
+
+const defaultHeaders = {
+  'Content-Type': 'application/json'
+}
+const sessionKey = 'fathom-auth-token'
+
+class CustomFetch {
+  #token?: string
+  fetch: typeof window.fetch
+  #originalFetch: typeof window.fetch
+
+  constructor() {
+    this.#originalFetch = window.fetch
+    this.#loadToken()
+    this.fetch = async (resource, options) => {
+      const req = new Request(resource, {
+        headers: { ...options?.headers, ...defaultHeaders, mode: 'cors' },
+        ...options
+      })
+      const destOrigin = new URL(req.url).origin
+      if (this.#token && allowedOrigins.includes(destOrigin)) {
+        req.headers.set('Authorization', this.#token)
+      }
+      return await this.#originalFetch(req)
+    }
+  }
+
+  setToken(token?: string) {
+    if (token) {
+      this.#token = token
+      sessionStorage.setItem(sessionKey, token)
+    }
+  }
+
+  #loadToken() {
+    const token = sessionStorage.getItem(sessionKey)
+    if (token) {
+      this.#token = token
+    }
+  }
+}
+
+export default new CustomFetch()
