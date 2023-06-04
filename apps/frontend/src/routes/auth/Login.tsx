d8f817582e2626c4e17fdf47d9678d815da1f329
andresyv <andresyv93@outlook.es>
2023-06-04 22:56:04 +0200

    FIX(Auth): fixed forms width

--- apps/frontend/src/routes/auth/Login.tsx
+++ apps/frontend/src/routes/auth/Login.tsx
@@ -2,7 +2,7 @@ import LoginForm from '../../components/forms/LoginForm'
 
 export default function LoginPage() {
   return (
-    <main className="flex h-screen bg-gray-50 w-screen px-10 justify-center items-center md:px-0">
+    <main className="flex h-screen bg-gray-50 w-screen px-4 md:px-10 justify-center items-center md:px-0">
       <LoginForm />
     </main>
   )
